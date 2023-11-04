97dccf1f6a7bc354c91d909b99d689c9997bf1ab
Aishwarya M <149547774+Aishwarya16920@users.noreply.github.com>
2023-11-04 13:43:56 +0530

    Add files via upload

---
+++ Java/Project 2/src/running_tasks_in_parallel/AppleTree.java
@@ -0,0 +1,36 @@
+package running_tasks_in_parallel;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+
+public class AppleTree {
+	// TODO Auto-generated method stub
+	
+	public static AppleTree[] newTreeGarden(int size) {
+		
+		AppleTree[] appleTrees = new AppleTree[size];
+		for(int i = 0; i < appleTrees.length; i++) {
+			appleTrees[i] = new AppleTree("Tree#" + i);
+		}
+		return appleTrees;
+	}
+	
+	private final String treeLabel;
+	private final int numberOfApples;
+	
+	public AppleTree(String treeLabel) {
+		this.treeLabel = treeLabel;
+		numberOfApples = 3;
+	}
+	
+	public int pickApples(String workerName) {
+		try {
+			System.out.printf("%s started picking apples from %s \n", workerName, treeLabel);
+			TimeUnit.SECONDS.sleep(1);
+			System.out.printf("%p picked %d apples from %s \n", workerName, numberOfApples, treeLabel);
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		return numberOfApples;
+	}
+}
---
+++ Java/Project 2/src/running_tasks_in_parallel/PickFruitsWithInvokeAll.java
@@ -0,0 +1,33 @@
+package running_tasks_in_parallel;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ForkJoinPool;
+
+public class PickFruitsWithInvokeAll {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AppleTree[] appleTrees = AppleTree.newTreeGarden(6);
+		
+		Callable<Void> applePicker1 = createApplePicker(appleTrees, 0, 2, "Alex");
+		Callable<Void> applePicker2 = createApplePicker(appleTrees, 2, 4, "Bob");
+		Callable<Void> applePicker3 = createApplePicker(appleTrees, 4, 6, "Carol");
+		
+		ForkJoinPool.commonPool().invokeAll(asList(applePicker1, applePicker2, applePicker3));
+
+		System.out.println();
+		System.out.println("All fruits collected!");
+		
+	}
+
+	public static Callable<Void> createApplePicker(AppleTree[] appleTrees, int fromIndexInclusive, int toIndexExclusive, String workerName){
+		return () -> {
+			for(int i = fromIndexInclusive; i < toIndexExclusive; i++) {
+				appleTrees[i].pickApples(workerName);
+			}
+			return null;
+		};
+	}
+
+}
